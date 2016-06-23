bf53e96d2637e1ceea29d5e4cd24da91b363a1b8
Bruno BOUTAREL <bruno.boutarel@wassa.fr>
2016-06-23 17:48:12 +0200

    Updated AndroidPush to send push in chunks of 750

--- Wassa/MPS/AndroidPush.php
+++ Wassa/MPS/AndroidPush.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Wassa\MPS;
+
 use Psr\Log\LoggerInterface;
 
 /**
@@ -13,6 +14,8 @@ use Psr\Log\LoggerInterface;
  */
 class AndroidPush extends AbstractPush
 {
+    const BATCH_CHUNK_SIZE = 750;
+
     /**
      * @inheritdoc
      */
@@ -26,41 +29,61 @@ class AndroidPush extends AbstractPush
      */
     public function sendPush()
     {
-        try
-        {
+        try {
             $gcmPhp = new \Wassa\GcmPhp\Gcm();
-            $response = $gcmPhp->sendMessage($this->parameters['api_key'],
-                $this->parameters['dry_run'],
-                $this->registrationTokens,
-                $this->pushData->getGcmPayloadData(),
-                $this->pushData->getGcmCollapseKey());
 
-            if (!$response) {
-                $this->logger->log('ERROR: Unable to parse response');
+            // Separate devices into chunks due to GCM limitation per request
+            $devicesChunks = array_chunk($this->registrationTokens, self::BATCH_CHUNK_SIZE);
 
-                return false;
-            }
-            else {
-                $okMessages = [];
-                $regIdsCount = count($this->registrationTokens);
+            $chunkResults = array();
+            foreach ($devicesChunks as $registrationsTokens) {
+                $response = $gcmPhp->sendMessage($this->parameters['api_key'],
+                    $this->parameters['dry_run'],
+                    $registrationsTokens,
+                    $this->pushData->getGcmPayloadData(),
+                    $this->pushData->getGcmCollapseKey());
 
-                for ($i = 0; $i < $regIdsCount; $i++) {
-                    $regId = $this->registrationTokens[$i];
-                    $responseItem = $response->results[$i];
+                if (!$response) {
+                    $this->logger->log('ERROR: Unable to parse response');
 
-                    if (isset($responseItem->message_id)) {
-                        $okMessages[] = $regId;
+                    return false;
+                } else {
+                    $okMessages = [];
+                    $regIdsCount = count($this->registrationTokens);
+
+                    for ($i = 0; $i < $regIdsCount; $i++) {
+                        $regId = $this->registrationTokens[$i];
+                        $responseItem = $response->results[$i];
+
+                        if (isset($responseItem->message_id)) {
+                            $okMessages[] = $regId;
+                        }
                     }
-                }
 
-                return $this->returnResult($okMessages, $this->registrationTokens, false);
+                    $chunkResults = $this->returnResult($okMessages, $this->registrationTokens, false);
+                }
             }
-        }
-        catch(\Exception $e)
-        {
+
+            // Merge chunks results into a single result
+            $result = array();
+            foreach ($chunkResults as $chunkResult) {
+                // If no result, set current chunkResult
+                if (empty($result)) {
+                    $result = $chunkResult;
+                } else {
+                    // Else merge chunk results with result
+                    $result['success'] = array_merge($result['success'], $chunkResult['success']);
+                    $result['error'] = array_merge($result['error'], $chunkResult['error']);
+                    $result['all_ok'] &= $chunkResult['all_ok'];
+                }
+            }
+            return $result;
+
+        } catch (\Exception $e) {
             //$this->failedMessages[] = $registrationId;
             $this->logger->log('ERROR: ' . $e->getMessage());
         }
+
 
         return $this->returnResult([], $this->registrationTokens, false);
     }
